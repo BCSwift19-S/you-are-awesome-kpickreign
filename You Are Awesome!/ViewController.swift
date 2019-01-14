861f96a6dd8aed429f7bd54a4bfdee09526dd7ed
Kelly Pickreign <kellypickreign@jkfnsjfkjfndair.bc.edu>
2019-01-13 19:45:36 -0500

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Kelly Pickreign on 1/13/19.
+//  Copyright © 2019 Kelly Pickreign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
